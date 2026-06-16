108c3bc7633d21ab2c23133a6891b6e2d05e08d5
kavyaneeli <kavyaneeli84@gmail.com>
2026-06-16 12:33:07 +0530

    first commit

--- script1.js
+++ script1.js
@@ -1,41 +1,45 @@
-let is24Hour = false;
+
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const toggleBtn = document.getElementById('toggle-btn')
-
-function updateClock(){
+let is24Hour = true;
+function updateTime(){
     const now = new Date();
 
     let hours = now.getHours();
     let minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    let seconds = now.getSeconds();
 
     let ampm = '';
 
     if (!is24Hour){
         ampm = hours >= 12? 'PM' :'AM';
         hours = hours % 12;
-        if (hours === 0) hours = 12;
+        hours = hours ? hours : 12;
     }
 hours = String(hours).padStart(2, '0');
     minutes= String(minutes).padStart(2, '0');
     seconds = String(seconds).padStart(2, '0');
 
-    timeEl.textContent = `${hours}:${minutes}:${seconds}:${ampm}`;
+    timeEl.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
 
     const day = String(now.getDate()).padStart(2, '0');
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const year = now.getFullYear();
-    dateEl.textContent = `${day}-${month}-${year}`;
+    dateEl.textContent = `${day}/${month}/${year}`;
 
     toggleBtn.addEventListener('click' , ()=>{
+        is24Hour = !is24Hour;
+        if (is24Hour){
+        toggleBtn.textContent ='Switch to 12-hour format';
+        }else{ 
+            toggleBtn.textContent =  'Switch to 24-hour format';
         
-        toggleBtn.textContent = is24Hour ? 'Switch to 12-hour format' : 'Switch to 24-hour format'
-        updateClock();
+        }
+        updateTime();
     });
-
-    setInterval(updateClock,1000);
-    updateClock();
     
     
 }
+updateTime();
+setInterval(updateTime, 1000);
